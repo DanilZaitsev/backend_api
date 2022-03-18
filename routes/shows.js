d132f19a1d33d1c889fa6a7c1d6853314e310e67
DanilZaitsev <99842025+DanilZaitsev@users.noreply.github.com>
2022-03-18 15:20:00 -0700

    Update shows.js

--- routes/shows.js
+++ routes/shows.js
@@ -17,7 +17,7 @@ router.get('/', function (req, res, next) {
 
         response_count++;
         if (response_count === 2)
-            res.send('EEEEEEE Obaldeti2' + JSON.stringify(merged));
+            res.send('Finished Obaldeti2' + JSON.stringify(merged));
     }
 
     const id = req.query.id ? req.query.id : '';
