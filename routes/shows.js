9e64305849dcab036a8352053800da88ee0fd162
DanilZaitsev <99842025+DanilZaitsev@users.noreply.github.com>
2022-05-27 14:24:34 -0700

    Look at that

--- routes/shows.js
+++ routes/shows.js
@@ -6,7 +6,7 @@ var request = require('request');
 router.get('/', function (req, res, next) {
     console.log(req.query);
     
-    return res.send('Hi All3');
+    return res.send('This is example');
     
     // EXAMPLE without Q -> less cpu usage
     let response_count = 0;
